d3cfb0045f7adca92cddf3fe1b3f40321a659582
Djavan Cackley <djavancackley@Djavans-MacBook-Pro.local>
2018-10-18 19:12:07 -1000

    #12 finished

--- js/app.js
+++ js/app.js
@@ -64,6 +64,8 @@ nameElem[0].innerHTML = 48;
 
 //12. In the div id matches section, target the first div element with the class name of status and change Status to Single Mother.
 
+var statElem = document.getElementsByClassName('status');
+statElem[0].innerHTML = 'Single Mother';
 //13. In the div id matches section, target the second div element with the class name of firstName and change Name to Peko Chan.
 
 //14. In the div id matches section, target the second div element with the class name of otherAge and change Age to 68.
